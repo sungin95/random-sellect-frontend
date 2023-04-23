7373a11f49cff015201860b38037f0dcc87e6f16
Sungin Lee <dlrkehrud@naver.com>
2023-04-23 14:26:30 +0900

    feat: 기본 화면 보기는거 까지

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+// import Cookie from "js-cookie";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+  withCredentials: true,
+});
+
+export const getMe = () =>
+  instance.get(`users/me`).then((response) => response.data);
