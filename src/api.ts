749c0d0f596ba0f3d91dd69fae0d579db778565a
Sungin Lee <dlrkehrud@naver.com>
2023-05-01 15:09:26 +0900

    fix: useQuery, tanstack!!!

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
-  withCredentials: true,
+  baseURL: "http://127.0.0.1:8000/api/v1",
+  //   withCredentials: true,
 });
 
 export const getMe = () =>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,23 +1,12 @@
-import { Box, HStack, VStack, useQuery } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 import { getLists } from "../api";
 import List from "../components/List";
 import { IList } from "../types";
-import { useEffect, useState } from "react";
 import ListSkeleton from "../components/ListSkeleton";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [rooms, setRooms] = useState<IList[]>([]);
-  const fetchRooms = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/questions/");
-    const json = await response.json();
-    setRooms(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    fetchRooms();
-  }, []);
-  //   const { isLoading, data } = useQuery<IList[]>(["lists"], getLists);
+  const { isLoading, data } = useQuery<IList[]>(["lists"], getLists);
   const countSkeleton = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
@@ -33,7 +22,7 @@ export default function Home() {
         </>
       ) : null}
       <Box w={"50%"}>
-        {rooms?.map((list) => (
+        {data?.map((list) => (
           <List
             key={list.pk}
             pk={list.pk}
