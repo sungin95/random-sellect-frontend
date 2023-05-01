7fe3d348e546f03c78cec3ded61aa424b357d3f0
Sungin Lee <dlrkehrud@naver.com>
2023-05-01 15:19:45 +0900

    refact: countSkeleton, for문으로 사용하여 만듬

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -7,9 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   const { isLoading, data } = useQuery<IList[]>(["lists"], getLists);
-  const countSkeleton = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const countSkeleton = [...Array(20)];
   return (
     <VStack>
       {isLoading ? (
