9aebe05d1771886394280e2f6c859b9aabb278a9
Sungin Lee <dlrkehrud@naver.com>
2023-05-03 16:08:15 +0900

    feat: start 완성, 새로고침현상만 수정하면 됨

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -6,63 +6,69 @@ import {
   Text,
   Textarea,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import { getMyListsStart } from "../api";
 import { IListMyChoice } from "../types";
 import ListSkeleton from "../components/ListSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function Start() {
   const { isLoading, data } = useQuery<IListMyChoice>(
     ["lists-my-choice-start"],
     getMyListsStart
   );
-  const countSkeleton = [];
-  for (let i = 1; i <= 20; i++) {
-    countSkeleton.push(i); // 리스트에 숫자 추가
-  }
-  //   const textRef = useRef<HTMLParagraphElement>(null);
-  //   const handleCopy = () => {
-  //     const textToCopy = textRef.current?.innerText;
-  //     if (textToCopy) {
-  //       navigator.clipboard.writeText(textToCopy);
-  //     }
-  //   };
+  const toast = useToast();
+  const [text, setText] = useState("");
+  let copyText = "";
+  const handleCopyClipBoard = async () => {
+    copyText =
+      '면접 상황을 가정했을 때 "' +
+      `${data?.description}` +
+      '"라는 질문에 이렇게 답했어 "' +
+      text +
+      '" 피드백 해줘';
+    await navigator.clipboard.writeText(copyText);
+    toast({
+      title: "복사되었습니다!",
+      status: "success",
+    });
+    window.open("https://chat.openai.com/");
+  };
+
   return (
     <VStack>
-      {isLoading ? (
-        <>
-          <Box w={"50%"}>
-            {countSkeleton.map((count) => (
-              <ListSkeleton key={count} />
-            ))}
-          </Box>
-        </>
-      ) : (
-        <Box w={"50%"}>
-          <HStack justifyContent={"center"} m={5}>
-            <Link to={"start"}>
-              <Button colorScheme="blue" fontSize={23}>
-                start
-              </Button>
-            </Link>
-          </HStack>
-          <HStack justifyContent={"space-between"} m={5}>
+      <Box w={"50%"}>
+        <HStack justifyContent={"center"} m={5}>
+          <Link to={"start"}>
+            <Button colorScheme="blue" fontSize={23}>
+              start
+            </Button>
+          </Link>
+        </HStack>
+        <HStack justifyContent={"space-between"} m={5}>
+          {isLoading ? (
+            <Text>loding...</Text>
+          ) : (
             <Text rounded="lg" width="60%" height={5} mb={1}>
               Q: {data?.description}
             </Text>
+          )}
+        </HStack>
+        <Divider />
+        <Box>
+          <Textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></Textarea>
+          <HStack justifyContent={"flex-end"}>
+            <Button mt={5} onClick={handleCopyClipBoard} colorScheme="blue">
+              복사
+            </Button>
           </HStack>
-          <Divider />
-          {/* <Box>
-            <Textarea cols={500} ref={textRef}></Textarea>
-            <HStack justifyContent={"flex-end"}>
-              <Button mt={5} onClick={handleCopy}>
-                복사
-              </Button>
-            </HStack>
-          </Box> */}
         </Box>
-      )}
+      </Box>
     </VStack>
   );
 }
