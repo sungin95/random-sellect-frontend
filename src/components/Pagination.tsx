1e65ad00514f6d141e12eb826b086302a9b490d9
Sungin Lee <dlrkehrud@naver.com>
2023-06-12 10:44:25 +0900

    feat: 페이지 네이션 5칸 보이게, 5칸 이동 - 페이지네이션을 1~5, 6~10... 보이게 만듬. - ...을 클릭하면 1 ->6으로 이동 or 6 -> 1로 이동 - 5칸이 넘지 않으면 보이지 않도록 설정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -9,7 +9,15 @@ export default function Pagination({ total, url }: IPaginNation) {
   const [page, setPage] = useState(1);
   const numPages = Math.ceil(total / 10);
   const pageArray = [];
-  for (let i = 1; i <= numPages; i++) {
+  const startPage = Math.floor((page - 1) / 5) * 5 + 1;
+  console.log(startPage);
+  let endPage = null;
+  if (numPages > startPage + 4) {
+    endPage = startPage + 4;
+  } else {
+    endPage = numPages;
+  }
+  for (let i = startPage; i <= endPage; i++) {
     pageArray.push(i); // 리스트에 숫자 추가
   }
   return (
@@ -20,6 +28,12 @@ export default function Pagination({ total, url }: IPaginNation) {
             &lt;
           </Button>
         </Link>
+        <Link to={`${url}${startPage - 5}`}>
+          <Button onClick={() => setPage(startPage - 5)} hidden={page === 1}>
+            &hellip;
+          </Button>
+        </Link>
+
         {pageArray.map((i) => (
           <Link to={`${url}${i}`}>
             <Button
@@ -31,6 +45,14 @@ export default function Pagination({ total, url }: IPaginNation) {
             </Button>
           </Link>
         ))}
+        <Link to={`${url}${startPage + 5}`}>
+          <Button
+            onClick={() => setPage(startPage + 5)}
+            hidden={endPage >= numPages}
+          >
+            &hellip;
+          </Button>
+        </Link>
         <Link to={`${url}${page + 1}`}>
           <Button onClick={() => setPage(page + 1)} hidden={page === numPages}>
             &gt;
